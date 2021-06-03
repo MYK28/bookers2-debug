1b7b71c8f73ae22ed39afbb19a102c54d1631106
MYK28 <myams1128@gmail.com>
2021-06-03 13:29:03 +0000

    [Add]Book-comment-validation

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,15 +2,24 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
+    # comment = BookComment.new(book_comment_params)
+    # comment.user_id = current_user.id
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book.id)
+    if comment.save
+    # redirect_to book_path(book.id)
+    redirect_to request.referer
+    else
+
+    redirect_back(fallback_location: root_path) #redirect_backはrenderの代わりに使用できる
+    flash[:notice]= '入力欄に記入してください。'
+    end
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    # redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
 
   private
