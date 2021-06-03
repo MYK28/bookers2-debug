23d93e70b579bf041c56a76ce2c1aa8b8334d188
MYK28 <myams1128@gmail.com>
2021-06-03 04:34:23 +0000

    [Add]Book-show-comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy #User.book_commentsで、ユーザーの所有するコメントを取得できる
+
   attachment :profile_image, destroy: false
 
   validates :name, uniqueness: true, length: {maximum: 20, minimum: 2}
